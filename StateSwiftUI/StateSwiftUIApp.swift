c73360d281cbda201768c197034ceed7253b4391
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-02-06 18:21:30 -0500

    Initial Commit

---
+++ StateSwiftUI/StateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateSwiftUIApp.swift
+//  StateSwiftUI
+//
+//  Created by Luis Angel Inga Mendoza on 6/02/23.
+//
+
+import SwiftUI
+
+@main
+struct StateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
