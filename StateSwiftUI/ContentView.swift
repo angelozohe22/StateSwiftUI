4d0d8fd55ef8b6ea2cf8858bd79ee81fc26bbbd3
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-02-21 12:47:17 -0500

    first commit

--- StateSwiftUI/ContentView.swift
+++ StateSwiftUI/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countLikes: Int = 0
+    @State private var isLiked: Bool = false
+    @State private var textWritten: String = ""
+    @State private var amount: Int = 0
+    @State private var amountString: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            Text(textWritten)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            VStack(spacing: 8) {
+                Button {
+                    isLiked.toggle()
+                    if isLiked {
+                        countLikes += 1
+                    }else {
+                        countLikes -= 1
+                    }
+                } label: {
+                    HStack {
+                        Image(systemName: !isLiked ? "heart" : "heart.fill")
+                            .foregroundColor(.red)
+                            .font(.largeTitle)
+                        Text(String(countLikes))
+                            .font(.body)
+                            .foregroundColor(.black)
+                    }
+                }
+                Button {
+                    amount += Int(amountString == "" ? "0" : amountString) ?? 0
+                } label: {
+                    HStack {
+                        Image(systemName: "dollarsign.circle.fill")
+                            .foregroundColor(.yellow)
+                            .font(.largeTitle)
+                        Text(String(amount))
+                            .font(.body)
+                            .foregroundColor(.black)
+                    }
+                }
+
+            }
+            TextField("Inserta un titulo", text: $textWritten)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.namePhonePad)
+            TextField("Inserta un monto", text: $amountString)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
         }
-        .padding()
+        .padding(.horizontal, 16)
     }
 }
 
